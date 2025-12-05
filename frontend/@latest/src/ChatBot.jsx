849c4c8fae40d9c357cb3cb3828fa36ba84cf12f
fopefoluwa <ojodorcas57@gmail.com>
2025-12-05 10:13:43 +0100

    new file added

--- frontend/@latest/src/ChatBot.jsx
+++ frontend/@latest/src/ChatBot.jsx
@@ -31,7 +31,7 @@ const ChatBot = () => {
       },
     ]);
     try {
-      const response = await fetch("http://127.0.0.1:8000/chat", {
+      const response = await fetch("https://my-chatbot-r9uo.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: msg }),
